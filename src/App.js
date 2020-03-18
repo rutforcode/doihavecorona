b5759e3fcf3779a5b86488d8350e9d184222bab5
deeayeen <dee.aye.en@gmail.com>
2020-03-18 02:38:18 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import MapComponent from "./components/MapComponent";
 import ReactGA from "react-ga";
 
-const trackingId = "UA-147414018-1"; // Replace with your Google Analytics tracking ID
+const trackingId = process.env.REACT_APP_GA; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
 
 function App() {
