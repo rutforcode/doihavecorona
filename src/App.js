74a9d154888611b11e7025c5065fbb9cf37e4e77
Dan Halperin <dee.aye.en@gmail.com>
2020-03-18 14:45:27 +0200

    trying to fix react ga

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./App.css";
 import MapComponent from "./components/MapComponent";
 import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 import ReactGA from "react-ga";
 import { BrowserView, MobileView } from "react-device-detect";
 
@@ -18,24 +19,28 @@ function App() {
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
   return (
-    <div>
-      <BrowserView>
-        <MapComponent />
-      </BrowserView>
-      <MobileView>
-        <div style={{ padding: "20px", fontWeight: "bold" }}>
-          <div>
-            This site is only available on desktop at the moment{" "}
-            <span role="img" aria-label="computer">
-              💻
-            </span>
-          </div>
-          <div style={{ marginTop: "24px" }}>
-            Please visit from your computer
-          </div>
-        </div>
-      </MobileView>
-    </div>
+    <Router history={browserHistory}>
+      <Switch>
+        <Route path="/">
+          <BrowserView>
+            <MapComponent />
+          </BrowserView>
+          <MobileView>
+            <div style={{ padding: "20px", fontWeight: "bold" }}>
+              <div>
+                This site is only available on desktop at the moment{" "}
+                <span role="img" aria-label="computer">
+                  💻
+                </span>
+              </div>
+              <div style={{ marginTop: "24px" }}>
+                Please visit from your computer
+              </div>
+            </div>
+          </MobileView>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
